9a3b345d659d3e4cb398847b90d656ff9243eba3
trandinhlinh94 <trandinhlinh94@gmail.com>
2015-09-04 13:45:39 +0300

    add the database connection and query classes

--- includes/database.php
+++ includes/database.php
@@ -2,12 +2,19 @@
 
 /**
  * @author Linh Tran
- * title: Database connection configuration
+ * title: Database connection and configurations 
  */
 
-function db_connect() {
+class db_connect {
 	// Define $connection as static to avoid connecting more than once 
-	static $connection;
+	protected static $connection;
+	/**
+     * Connect to the database
+     * 
+     * @return bool false on failure / mysqli MySQLi object instance on success
+     */
+
+	public function connect() {
 
 	// Try to connect to the database if a connection has not been established yet 
 	if(isset($connection)) {
@@ -23,6 +30,68 @@ function db_connect() {
 		return mysqli_connect_error();
 	}
 	return $connection;
+	}
+
+	/**
+     * Query the database
+     *
+     * @param $query The query string
+     * @return mixed The result of the mysqli::query() function
+     */
+
+	public function query($query) {
+		//connect to database
+		$connection = $this -> connect();
+
+		// query the database
+		$result = connection() -> query($query);
+		return $result;
+	}
+
+	 /**
+     * Fetch rows from the database (SELECT query)
+     *
+     * @param $query The query string
+     * @return bool False on failure / array Database rows on success
+     */
+	 public function select($query){
+     $row = array();
+     $result =  $this ->query($query);
+     
+     if($result === false) {
+     	return false;
+     }
+
+     while($row = $result -> fetch_assoc() ){
+     	$row[] = $row;
+     }
+
+     return $row;
+ 	}
+	
+	/**
+     * Fetch the last error from the database
+     * 
+     * @return string Database error message
+     */
+
+	public function error() {
+		$connection = $this -> connect();
+		return $connection -> error();
+	}
+
+	/**
+     * Quote and escape value for use in a database query
+     *
+     * @param string $value The value to be quoted and escaped
+     * @return string The quoted and escaped string
+     */
+
+	public function quote($value) {
+		$connection = $this -> connect();
+		return "'" . $connection -> real_escape_string($value) . "'" ;
+	}
 }
+
 
 ?>
